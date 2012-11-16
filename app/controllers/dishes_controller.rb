98d3b9e28be642b06c2883d28ae4e4fc3456d98c
Chris Goodmacher <cgoodmac@gmail.com>
2012-11-16 12:16:06 -0500

    dishes done

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,16 +1,45 @@
 class DishesController < ApplicationController
 	def index
+		@dishes = Dish.all
 	end
 
 	def show
+		dish_id = params[:id]
+		@dish = Dish.find(dish_id)
 	end
 
 	def new
+		@dish = Dish.new
 	end
 
 	def edit
+		@dish = Dish.find(params[:id])
 	end
 
-	def delete
+	def create
+		@dish = Dish.new(params[:dish])
+
+		if @dish.save
+			redirect_to dishes_path
+		else
+			render :new
+		end
+
+	end
+
+	def update
+		@dish = Dish.find(params[:id])
+
+		if @dish.update_attributes(params[:dish])
+			redirect_to @dish
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		dish = Dish.find(params[:id])
+		dish.delete
+		redirect_to dishes_path
 	end
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: dishes
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  cost       :decimal(, )
+#  photo      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Dish < ActiveRecord::Base
   validates :name, :photo, :presence => true
   validates :cost, :numericality => {:greater_than => 10}

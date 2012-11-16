ebb23ac988f89497e21704d0e4d613fac9905f59
Jonathan Ku <jonathanku88@gmail.com>
2012-11-16 11:44:48 -0500

    changes

---
+++ app/models/restaurant.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  cuisine    :string(255)
+#  address    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Restaurant < ActiveRecord::Base
+	validates :cuisine, :presence => true
+	validates :address, :presence => true
+	validates_length_of :name, :minimum => 3
+
+	has_many :dishes	
+end
